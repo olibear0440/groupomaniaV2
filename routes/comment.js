dc552e357066e7e124b50c65fa8931624af493d2
olivierbenoit <olivier.bent@gmail.com>
2021-08-23 23:20:03 +0200

    test crud + frontend html accueil

--- routes/comment.js
+++ routes/comment.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const commentCtrl = require("../controllers/comment");
-const auth = require("../middleware/auth");
+//const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 
-router.get("/", auth, commentCtrl.getComments);
-router.post("/", auth, multer, commentCtrl.createComment);
+router.get("/", commentCtrl.getComments);
+router.post("/", multer, commentCtrl.createComment);
 
 module.exports = router;
--- routes/post.js
+++ routes/post.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const postCtrl = require("../controllers/post");
 const multer = require("../middleware/multer-config");
-const auth = require("../middleware/auth");
+//const auth = require("../middleware/auth");
 
-router.get("/", auth, postCtrl.getPosts);
-router.get("/:id", auth, postCtrl.getOnePost);
-router.post("/", auth, multer, postCtrl.createPost);
+//probleme avec le middleware de auth, une fois integré dans les routes plus rien ne s'affiche sur le test du fetch
+router.get("/", postCtrl.getPosts);
+router.get("/:id", postCtrl.getOnePost);
+router.post("/", multer, postCtrl.createPost);
 
 module.exports = router;
